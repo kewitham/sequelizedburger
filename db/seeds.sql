e0c115fe11e7d0d195d969dd6480d066b6275712
Katie Witham <kewitham@gmail.com>
2016-10-10 15:21:08 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beets Me Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bean Spirit Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Summer Thyme Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tunami', true);
